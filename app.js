b88ed5111406dcfbcc7e168e54efeb062587e459
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-09-14 02:16:04 +0100

    Add js

---
+++ app.js
@@ -0,0 +1,86 @@
+////////////////////////////////////
+//////// BUDGET CONTROLLER
+////////////////////////////////////
+var budgetController = (function() {
+
+
+    return {
+
+    };
+}());
+
+
+
+
+
+
+
+
+////////////////////////////////////
+//////// UI CONTROLLER
+////////////////////////////////////
+var UIController = (function() {
+
+    var DOMstrings = {
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addButton: '.add__btn',
+    };
+
+    return {
+    getInput = function() {
+        return {
+            description: document.querySelector(DOMstrings.inputDescription).value;
+            value: document.querySelector(DOMstrings.inputValue).value;
+            //////////////////////////////////////////////////////////////////    Add Type of item
+    }
+
+    getDOMstrings: function() {
+        return DOMstrings;
+    };
+
+};
+}());
+
+
+////////////////////////////////////
+//////// APP CONTROLLER
+////////////////////////////////////
+var appController = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.addButton).addEventListener('click', addItem);
+
+        document.addEventListener('keypress', function(e) {
+            if (e.keyCode === 13 || e.which === 13) {
+                console.log('Enter')
+            };
+        });
+    };
+
+    var addItem = function() {
+        console.log('Button pressed!');
+        // Get the filled input data.
+        UICtrl.getInput();
+
+        // Add item to the budget controller
+
+        // Add the item to the UI
+
+        // Calculate the budget
+
+        // Display the budget on the UI
+    }
+
+
+
+    return {
+        init: function() {
+            setupEventListeners();
+            console.log('App has started');
+    };
+};
+}(budgetController, UIController));
+
+appController.init()
