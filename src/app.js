09cec4d553f3b0a971fc4c326197e8fc58d3adf1
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-19 02:04:20 +0100

    Set webpack/babel - continue.

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-import {stri} from './abc.js';
-import {Expense} from './abc.js';
-
-console.log(stri);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebapckPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+      contentBase: './dist'
+  },
+  plugins: [
+      new HtmlWebapckPlugin({
+          filename: 'index.html',
+          template: './src/index.html'
+      })
+  ],
   module: {
     rules: [
       {
